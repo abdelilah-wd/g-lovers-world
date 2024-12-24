e4331794f5a010a6fa736185d26d10191d452335
abdelilah-wd <abdelilahmissa0@gmail.com>
2024-12-24 20:12:45 +0100

    upload the design

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ function sendToWhatsApp(productName, productImage) {
     window.open(url, "_blank");
 }
 
+function sendEmail(productName, productImage) {
+    const email = "contactme.abdelilah@gmail.com"; // عنوان البريد
+    const subject = `more details about product: ${productName}`;
+    const body = `hello ,%0A%0Ai need more details about this product: ${productName}.%0A img link: ${productImage}%0A%0Aشكرًا لكم!`;
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.location.href = mailtoLink;
+}
+
